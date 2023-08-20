03fcb1675d722b68b50c629440b9f1bdb82bacd9
Christopher Lynde <lyndechris@gmail.com>
2023-08-19 20:43:55 -0400

    Client Manager
    -Created clientManager class to include in any other files that require client interaction
    -Move bulk of client code from index.js to clientManager.js
    -Updated voiceStateUpdate event to only log if loggingState for that channel is set to true

---
+++ clientManager.js
@@ -0,0 +1,73 @@
+require("dotenv").config();
+const { Client, Collection, GatewayIntentBits } = require("discord.js");
+const fs = require("fs");
+const path = require("path");
+
+class ClientManager {
+	constructor() {
+		this.client = new Client({
+			intents: [
+				GatewayIntentBits.Guilds,
+				GatewayIntentBits.GuildMessages,
+				GatewayIntentBits.MessageContent,
+				GatewayIntentBits.GuildMembers,
+				GatewayIntentBits.GuildVoiceStates,
+			],
+		});
+
+		this.client.commands = new Collection();
+		this.client.loggingStates = new Collection();
+
+		this.loadCommands();
+	}
+
+	async login(token) {
+		await this.client.login(token);
+	}
+
+	getClient() {
+		return this.client;
+	}
+
+	loadCommands() {
+		const commandsPath = path.join(__dirname, "commands");
+		const commandFiles = fs
+			.readdirSync(commandsPath)
+			.filter((file) => file.endsWith(".js"));
+
+		for (const file of commandFiles) {
+			const filePath = path.join(commandsPath, file);
+			const command = require(filePath);
+			if ("data" in command && "execute" in command) {
+				this.client.commands.set(command.data.name, command);
+			} else {
+				console.log(
+					`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+				);
+			}
+		}
+	}
+
+	loadEvents() {
+		const eventsPath = path.join(__dirname, "events");
+		const eventFiles = fs
+			.readdirSync(eventsPath)
+			.filter((file) => file.endsWith(".js"));
+
+		for (const file of eventFiles) {
+			const filePath = path.join(eventsPath, file);
+			const event = require(filePath);
+			if (event.once) {
+				this.client.once(event.name, (...args) =>
+					event.execute(...args, this.client)
+				);
+			} else {
+				this.client.on(event.name, (...args) =>
+					event.execute(...args, this.client)
+				);
+			}
+		}
+	}
+}
+
+module.exports = new ClientManager();
--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,15 +1,32 @@
+const clientManager = require("../clientManager");
+const client = clientManager.getClient();
 const { Events } = require("discord.js");
 
 module.exports = {
 	name: Events.VoiceStateUpdate,
 	execute(oldState, newState) {
+		// Check to see if logging is active for channel
+		const guildId = newState.guild.id;
+		const channelId = newState.channelId || oldState.channelId;
+		const loggingStates = client.loggingStates;
+
+		if (
+			!loggingStates.has(guildId) ||
+			!loggingStates.get(guildId).has(channelId)
+		) {
+			return;
+		}
+
+		const channelStates = client.loggingStates.get(guildId);
+		if (!channelStates.get(channelId)) {
+			return;
+		}
+
 		const user = newState.member.user.tag;
 		const channel = newState.channel
 			? newState.channel.name
 			: oldState.channel.name;
 		const timestamp = new Date().toLocaleString();
-		// const guildId = newState.guild.id;
-		// const channelId = newState.channelId || oldState.channelId;
 
 		if (!oldState.channel && newState.channel) {
 			console.log(`${timestamp}: ${user} joined ${channel}.`);
