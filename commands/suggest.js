2ff63ceac558534831358d35e913a94875fde46c
Christopher Lynde <lyndechris@gmail.com>
2023-08-25 14:34:27 -0400

    Listener for suggestedShowsList
    -Added an event listener for client.suggestedShowsList updates which will handle editing the suggestions embed

--- commands/suggest.js
+++ commands/suggest.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const clientManager = require("../clientManager");
 const client = clientManager.getClient();
-const { EmbedBuilder, SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -21,32 +21,12 @@ module.exports = {
 
 		client.suggestedShowsList.push(suggestedShow);
 
-		const reply = await interaction.reply("suggesting");
+		const reply = await interaction.reply({
+			content: "suggesting",
+			ephemeral: true,
+		});
 		reply.delete();
 
-		// Only build embed if watchlist is currently displaying
-		if (client.appStates.get("wl")) {
-			const suggestedShowsEmbedStruct = client.embeds.get(
-				"suggestedShowsEmbedStruct"
-			);
-			const suggestedShowsEmbed = suggestedShowsEmbedStruct[0];
-			const suggestedShowsEmbedMsg = suggestedShowsEmbedStruct[1];
-			const newEmbed = EmbedBuilder.from(suggestedShowsEmbed)
-				.setDescription("\u200B")
-				.addFields({
-					name: suggestedShow,
-					value: "1 vote",
-					inline: true,
-				});
-			// client.embeds.set("suggestedShowsEmbedStruct", newEmbed);
-
-			await suggestedShowsEmbedMsg
-				.edit({
-					embeds: [newEmbed],
-				})
-				.then((msg) => {
-					client.embeds.set("suggestedShowsEmbedStruct", [newEmbed, msg]);
-				});
-		}
+		client.emit("suggestionsUpdate");
 	},
 };
---
+++ events/suggestionsUpdate.js
@@ -0,0 +1,35 @@
+const clientManager = require("../clientManager");
+const client = clientManager.getClient();
+const {
+	ActionRowBuilder,
+	ButtonBuilder,
+	ButtonStyle,
+	EmbedBuilder,
+} = require("discord.js");
+
+module.exports = {
+	name: "suggestionsUpdate",
+	async execute(client) {
+		// watchlist is active
+		if (client.appStates.get("wl")) {
+			const [embed, msg] = client.embeds.get("suggestedShowsEmbedStruct");
+			const descHeader = `----------------------------------------------------------\n`;
+			const emptyHeader = `Currently empty.`;
+			let desc = client.suggestedShowsList.length
+				? descHeader
+				: descHeader + emptyHeader;
+			for (let i = 0; i < client.suggestedShowsList.length; i++) {
+				desc += client.suggestedShowsList[i] + "\n";
+			}
+			const newEmbed = EmbedBuilder.from(embed).setDescription(desc);
+
+			await msg
+				.edit({
+					embeds: [newEmbed],
+				})
+				.then((msg) => {
+					client.embeds.set("suggestedShowsEmbedStruct", [newEmbed, msg]);
+				});
+		}
+	},
+};
