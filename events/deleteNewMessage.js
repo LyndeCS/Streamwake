7824d14a2ac91ff394154e100a7b06d1acb30c7f
Christopher Lynde <lyndechris@gmail.com>
2023-08-27 00:02:35 -0400

    Delete chat messages
    -Bot will delete all new messages that are not its own in specified channel

---
+++ events/deleteNewMessage.js
@@ -0,0 +1,14 @@
+const clientManager = require("../clientManager");
+const client = clientManager.getClient();
+const { Events } = require("discord.js");
+
+module.exports = {
+	name: Events.MessageCreate,
+	async execute(message) {
+		if (message.channel.id === "1140355793418457118") {
+			if (message.author.id !== client.user.id) {
+				await message.delete();
+			}
+		}
+	},
+};
