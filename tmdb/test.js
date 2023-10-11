ac9270bb1c32a3d174f8b70822335c3551efaa5b
Christopher Lynde <lyndechris@gmail.com>
2023-10-10 23:49:36 -0400

    test anime show extraction

--- tmdb/test.js
+++ tmdb/test.js
@@ -5,32 +5,36 @@ const path = require("path");
 const TMDB_RAT = `Bearer ${process.env.TMDB_RAT}`;
 
 const url =
-	"https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=true&language=en-US&sort_by=primary_release_date.asc&with_genres=anime";
+	"https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=true&page=1&sort_by=primary_release_date.desc&with_genres=anime";
 const options = {
 	method: "GET",
 	headers: {
 		accept: "application/json",
 		Authorization:
-			"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NzcxNDYwYTllY2YyMzIwZmNjMWE4YWVmZjkyYmI0ZiIsInN1YiI6IjY1MjNiNTE2ZWE4NGM3MDEwYzE4MzUxNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.m8qcrCLWglPvLeVR1KJeCH8cGjCNgxwUPP0l9azd1iA",
+			TMDB_RAT,
 	},
 };
 
 fetch(url, options)
 	.then((res) => res.json())
 	.then((json) => {
-		console.log(json);
+		console.log(json.results[0]["name"]);
 
-		// Log to a file
-		let testMessage = JSON.stringify(json);
-		const testFilePath = path.join(__dirname, "..", "test", "test.log");
+        for (const result in results){
 
-		// Create the directory if it doesn't exist
-		const testDirectory = path.join(__dirname, "..", "test");
-		if (!fs.existsSync(testDirectory)) {
-			fs.mkdirSync(testDirectory);
-		}
+        }
 
-		// Append message to the file
-		fs.appendFileSync(testFilePath, testMessage);
+		// // Log to a file
+		// let testMessage = JSON.stringify(json);
+		// const testFilePath = path.join(__dirname, "..", "test", "test.log");
+
+		// // Create the directory if it doesn't exist
+		// const testDirectory = path.join(__dirname, "..", "test");
+		// if (!fs.existsSync(testDirectory)) {
+		// 	fs.mkdirSync(testDirectory);
+		// }
+
+		// // Append message to the file
+		// fs.appendFileSync(testFilePath, testMessage);
 	})
 	.catch((err) => console.error("error:" + err));
