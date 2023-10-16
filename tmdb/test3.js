7eaea6272baaa9de62c447d979b3b9bf115d616e
Christopher Lynde <lyndechris@gmail.com>
2023-10-16 18:33:11 -0400

    investigating season/episode_groups

--- tmdb/test3.js
+++ tmdb/test3.js
@@ -14,8 +14,7 @@ const limiter = new Bottleneck({
 	minTime: 1000, // Minimum time (in milliseconds) between requests
 });
 
-const base_url =
-	"https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=true&sort_by=primary_release_date.asc&with_genres=16&with_keywords=210024";
+const base_url = "https://api.themoviedb.org/3/tv/";
 const options = {
 	method: "GET",
 	headers: {
@@ -48,6 +47,25 @@ const recently_watched_ids = [
 	"60863",
 ];
 
-sequelize.authenticate().then(async () => {
-	for (const i = 0; i < recently_watched_ids.length; ) {}
-});
+sequelize
+	.authenticate()
+	.then(() => {
+		// for (const i = 0; i < recently_watched_ids.length; i++) {}
+		const url = `${base_url}95479`;
+		const episode_group = `https://api.themoviedb.org/3/tv/episode_group/{tv_episode_group_id}`;
+		const episode_url = `https://api.themoviedb.org/3/tv/95479/season/2/episode/1`;
+		fetch(url, options)
+			.then((res) => res.json())
+			.then((show) => {
+				// console.log(`Name: ${show.name}`);
+				// console.log(`Seasons: ${show.number_of_seasons}`);
+				// console.log(`Episodes: ${show.number_of_episodes}`);
+				console.log(show.seasons);
+			})
+			.catch((err) => {
+				console.error(`${err}`);
+			});
+	})
+	.catch((error) => {
+		console.error("Unable to connect to the database: ", error);
+	});
