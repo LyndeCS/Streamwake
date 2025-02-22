b50407c0efe434c78fa9d7f4275dfdc9bce6f7d5
Christopher Lynde <lyndechris@gmail.com>
2025-02-22 00:07:36 -0500

    Bugfix: scheduledEvent
    scheduled event description now correctly updates description with watchlist

--- events/scheduledEventCreated.js
+++ events/scheduledEventCreated.js
@@ -1,5 +1,4 @@
 const clientManager = require("../clientManager");
-const client = clientManager.getClient();
 const { Events } = require("discord.js");
 
 module.exports = {
@@ -7,24 +6,43 @@ module.exports = {
 	async execute(guildScheduledEvent) {
 		// Check if "Animaturday" is mentioned in the event name, case-insensitive
 		console.log(`Event created: ${guildScheduledEvent.name}`);
+		try {
+			if (guildScheduledEvent.name.toLowerCase().includes("animaturday")) {
+				const sortedWatchlist = await clientManager
+					.getWatchlist()
+					.sort((a, b) => (a.position > b.position ? 1 : -1));
+				const maxShowTitleLength = Math.max(
+					...sortedWatchlist.map((item) => item.show_name.length)
+				);
 
-		if (guildScheduledEvent.name.toLowerCase().includes("animaturday")) {
-			// Fetch the watchlist from the database
-			const watchlistItems =
-				await clientManager.sequelize.models.watchlist.findAll();
+				let output = "```\n";
+				output += `#  | ${"Show Title".padEnd(maxShowTitleLength)} | S | E\n`;
 
-			let eventDescription = "";
-			watchlistItems.forEach((item) => {
-				// eventDescription += `- ${item.show_name} - S${item.season_number}E${item.episode_number}: ${item.episode_name}\n`;
-                                eventDescription += `- ${item.show_name}: S${item.season_number}E${item.episode_number}\n`;
-			});
+				output += `---+${"-".repeat(maxShowTitleLength + 2)}+---+---\n`;
 
-			// Update the event description
-			await guildScheduledEvent.edit({
-				description: eventDescription,
-			});
+				for (const item of sortedWatchlist) {
+					output += `${item.position}. | ${item.show_name.padEnd(
+						maxShowTitleLength
+					)} | ${item.season_number} | ${item.episode_number}\n`;
+				}
 
-			console.log(`Updated description for event: ${guildScheduledEvent.name}`);
+				output += "```";
+				let eventDescription = ``;
+				sortedWatchlist.forEach((item) => {
+					eventDescription += `${item.position}. ${item.show_name}: s${item.season_number} e${item.episode_number}\n`;
+				});
+
+				// Update the event description
+				await guildScheduledEvent.edit({
+					description: eventDescription,
+				});
+
+				console.log(
+					`Updated description for event: ${guildScheduledEvent.name}`
+				);
+			}
+		} catch (error) {
+			console.error("Error updating event description:", error);
 		}
 	},
 };
